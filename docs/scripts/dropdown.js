bfb62ea054ca39600cdb58374f2d389812213af8
Zene <oinkymail3@gmail.com>
2022-04-02 09:05:56 +0100

    Fixed styling and dropdown issues in docs

--- docs/scripts/dropdown.js
+++ docs/scripts/dropdown.js
@@ -12,6 +12,20 @@ function addFocusEvent(target)
     }, true);
 }
 
+function addHoverEvent(target)
+{
+    target.addEventListener("mouseenter", function(event)
+    {
+        target.children[1].style.transform = "scaleY(100%)";
+        
+    }, true);
+    
+    target.addEventListener("mouseleave", function(event)
+    {
+        target.children[1].style.transform = "scaleY(0%)";
+    }, true);
+}
+
 var dropdowns = document.getElementsByClassName("nav-dropdown");
 
 for (var n = 0; n < dropdowns.length; n++)
@@ -20,4 +34,5 @@ for (var n = 0; n < dropdowns.length; n++)
     if (dropdowns[n].children.length == 0) { continue; }
     
     addFocusEvent(dropdowns[n]);
+    addHoverEvent(dropdowns[n]);
 }
